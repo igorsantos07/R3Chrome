db8fbacd9fadf323e6a75ba17c5c493d9f8b2ae2
Igor Santos <igorsantos07@gmail.com>
2012-12-06 02:31:56 -0400

    + trying to add fucking internationalization, but it never works in the dev dashboard. refs #1

--- r3.js
+++ r3.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+	var title = chrome.i18n.getMessage('title')
+	document.getElementsByTagName('title')[0].innerHTML = title
+	document.getElementsByTagName('legend')[0].innerHTML = title
+
 	var num1	= document.getElementById('num1'),
 		num2	= document.getElementById('num2'),
 		num3	= document.getElementById('num3'),
